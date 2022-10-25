35b1a667d6ed38c7a5f5a1820f01386ee058d007
Alan Soares <alanrsoars@gmail.com>
2022-10-25 20:49:27 +1300

    v1.0.17

--- scripts/postversion.mjs
+++ scripts/postversion.mjs
@@ -7,4 +7,4 @@ const { stdout } = await $`cat package.json | jq  -r .version`;
 const document = JSON.stringify({ version: stdout.trim() }, null, 2);
 
 await $`echo ${document} > ./static/meta.json`;
-await $`git add . && git commit --ammend --no-edit`;
+await $`git add . && git commit --amend --no-edit`;
