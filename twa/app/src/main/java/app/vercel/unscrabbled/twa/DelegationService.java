4763b15bc6b14b5fee675fd6e9da02596c81edbd
Alan Soares <alanrsoars@gmail.com>
2023-06-12 11:18:22 +1200

    feat: add twa

---
+++ twa/app/src/main/java/app/vercel/unscrabbled/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.unscrabbled.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
